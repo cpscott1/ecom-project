f5d4c99d75b3be7de76cf590e6b6a0c2aa4527c0
cpscott1 <thacam18@gmail.com>
2019-11-03 21:25:04 -0600

    finished styling menu item component

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -43,8 +43,8 @@ const Directory = () => {
 return (
   <div className='directory-menu'>
   {
-    sections.map(({title, imageUrl, id}) => (
-      <MenuItem key={id} title={title}/>
+    sections.map(({title, imageUrl, id, size}) => (
+      <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
     ))
   }
   </div>
